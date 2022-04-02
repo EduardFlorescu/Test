71860e0072d63a78895a42a5ca895cf4c6c7ddd9
DESKTOP-NAKD67N\Edi <fl_eduard@yahoo.com>
2022-04-03 00:14:48 +0300

    Add initial project

---
+++ Stuffs/Stuffs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Stuffs
+{
+    class Program
+    {
+        public static int Addition( int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Substraction(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int Multiplication(int x, int y)
+        {
+            return x * y;
+        }
+
+        public static double Division(int x, int y)
+        {
+            return x / y;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Addition(2,3));
+            Console.WriteLine(Substraction(2,3));
+            Console.WriteLine(Multiplication(2,3));
+            Console.WriteLine(Division(2,3));
+        }
+    }
+}
